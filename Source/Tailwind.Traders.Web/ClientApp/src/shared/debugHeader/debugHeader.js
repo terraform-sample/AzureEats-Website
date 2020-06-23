de4c14ea66260a4c6659c2329f490eca7dc420c3
Jesus Estevez <jecaestevez@gmail.com>
2020-06-23 18:36:29 +0200

    fix js

--- Source/Tailwind.Traders.Web/ClientApp/src/shared/debugHeader/debugHeader.js
+++ Source/Tailwind.Traders.Web/ClientApp/src/shared/debugHeader/debugHeader.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import { ConfigService } from "../../services";
 import { withNamespaces } from "react-i18next";
 
